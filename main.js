d07298ea9d76d1f59929e930f9a3a4fb5e8d8f76
Domonkos Anton <you@example.com>
2017-11-15 19:16:57 +0100

    :checkered_flag: Inital Play/Pause support on Windows via Keybindings

--- main.js
+++ main.js
@@ -1,55 +1,60 @@
  // Handling squirrel setupevents before the process starts.
  const setupEvents = require('./build_tools/setupEvents')
  if (setupEvents.handleSquirrelEvent()) {
-    // squirrel event handled and app will exit in 1000ms, so don't do anything else
-    return;
+     // squirrel event handled and app will exit in 1000ms, so don't do anything else
+     return;
  }
 
-const {app, BrowserWindow, ipcMain, shell} = require('electron');
-const url = require('url');
+ const { app, BrowserWindow, ipcMain, shell } = require('electron');
+ const url = require('url');
 
-let mainWindow;
+ let mainWindow;
 
-app.on('window-all-closed', () => {
-  // On OS X it is common for applications and their menu bar
-  // to stay active until the user quits explicitly with Cmd + Q
-  if (process.platform != 'darwin') {
-    app.quit();
-  }
-});
+ app.on('window-all-closed', () => {
+     // On OS X it is common for applications and their menu bar
+     // to stay active until the user quits explicitly with Cmd + Q
+     if (process.platform !== 'darwin') {
+         app.quit();
+     }
+ });
 
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-app.on('ready', function() {
-  mainWindow = new BrowserWindow({width: 1000, height: 800});
+ // This method will be called when Electron has finished
+ // initialization and is ready to create browser windows.
+ app.on('ready', function() {
+     mainWindow = new BrowserWindow({ width: 1000, height: 800, show: false });
 
-  mainWindow.loadURL('file://' + __dirname + '/dist/index.html');
-  try {require('./app/scripts/mpris').init(app, mainWindow);} catch (err) {}
-  try {require('electron-connect').client.create(mainWindow);} catch (err) {}
-  // Emitted when the window is closed.
-  mainWindow.on('closed', () => {
-    // Dereference the window object, usually you would store windows
-    // in an array if your app supports multi windows, this is the time
-    // when you should delete the corresponding element.
-    mainWindow = null;
-  });
+     mainWindow.loadURL('file://' + __dirname + '/dist/index.html');
+     try { require('./app/scripts/mpris').init(app, mainWindow); } catch (err) {}
+     try { require('electron-connect').client.create(mainWindow); } catch (err) {}
 
-  mainWindow.webContents.on('new-window', function (e, uri) {
-    let protocol = url.parse(uri).protocol;
+     // Emitted when the window is closed.
+     mainWindow.on('closed', () => {
+         // Dereference the window object, usually you would store windows
+         // in an array if your app supports multi windows, this is the time
+         // when you should delete the corresponding element.
+         mainWindow = null;
+     });
 
-    if (protocol === 'http:' || protocol === 'https:') {
-      e.preventDefault();
-      shell.openExternal(uri);
-    }
-  })
+     mainWindow.webContents.on('new-window', (e, uri) => {
+         let protocol = url.parse(uri).protocol;
 
-  ipcMain.on('foreground', (e, arg) => {
-    mainWindow.setAlwaysOnTop(arg);
-    e.returnValue = arg;
-  });
+         if (protocol === 'http:' || protocol === 'https:') {
+             e.preventDefault();
+             shell.openExternal(uri);
+         }
+     })
 
-  ipcMain.on('stationChange', (e, arg) => {
-    mainWindow.setIcon(__dirname + '/dist/images/logos/' + arg + '-64.png');
-    e.returnValue = arg;
-  });
-});
+     mainWindow.once('ready-to-show', () => {
+         mainWindow.show();
+     })
+
+     ipcMain.on('foreground', (e, arg) => {
+         mainWindow.setAlwaysOnTop(arg);
+         e.returnValue = arg;
+     });
+
+     ipcMain.on('stationChange', (e, arg) => {
+         mainWindow.setIcon(__dirname + '/dist/images/logos/' + arg + '-64.png');
+         e.returnValue = arg;
+     });
+ });
